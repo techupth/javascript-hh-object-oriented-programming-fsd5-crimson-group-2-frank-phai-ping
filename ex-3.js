44a0368e9f7a89cca09892df16fe35a4160f8859
nattawat <frank.nattawat@hotmail.com>
2023-12-17 16:14:35 +0700

    feat:complete ex

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,78 @@
 //  Start coding here
+class User{
+    constructor(id, name, email){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+}
+
+class postlist{
+    constructor(posts){
+        this.posts = [];
+    }
+    addPost(post){
+        this.post.push(post);
+    }
+    sharedPost(postTitle){
+        console.log(`You've shared post ${postTitle}`)
+    }
+}
+
+class Post{
+    constructor(id, title, content, comment){
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.comment = [];
+    }
+    addComment(textComment){
+        this.comment.push(textComment);
+    }
+}
+
+class Comment{
+    constructor(id, content, createBy, like){
+        this.id = id;
+        this.content = content;
+        this.createBy = createBy;
+        this.like = 0;
+    }
+    addLike(like){
+        this.like += like;
+    }
+}
+
+class Facebook{
+    constructor(groupList, pageList){
+        this.groupList = [];
+        this.pageList = [];
+    }
+    addGroup(group){
+        this.groupList.push(group);
+    }
+    addPage(page){
+        this.pageList.push(page);
+    }
+}
+
+class FacebookPage{
+    constructor(name){
+        this.name = name;
+    }
+}
+
+class FacebookGroup{
+    constructor(name){
+        this.name = name;
+    }
+}
+
+class Notification{
+    constructor(id){
+        this.id = id;
+    }
+    send(outPut){
+        console.log(`Notification: ${Comment.this.createBy} has just commented on this post—${Post.this.title}`)
+    }
+}
